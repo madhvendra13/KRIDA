096a8542f4fcbb9e43f489c652a0f03bca80c877
Madhvendra Singh Chouhan <chouhanmadhvendra13@gmail.com>
2024-08-29 18:44:30 +0530

    files updated

--- my-krida-app/src/components/Navbar.jsx
+++ my-krida-app/src/components/Navbar.jsx
@@ -1,16 +1,16 @@
     // src/components/Navbar.jsx
-import React from 'react';
+    import React from 'react';
 
-function Navbar() {
-  return (
-    <div className="navbar">
-    <div className="game-name-container">Game Name</div>
-    <div className="navbar-title"> क्रीडा</div>
-    <div className="user-profile-container">Username</div>
-  </div>
-  
-
-  );
-}
-
-export default Navbar;  // Make sure this line is present
+    function Navbar() {
+      return (
+        <div className="navbar">
+        <div className="game-name-container">Game Name</div>
+        <div className="navbar-title"> क्रीडा</div>
+        <div className="user-profile-container">Username</div>
+      </div>
+      
+    
+      );
+    }
+    
+    export default Navbar;  // Make sure this line is present
